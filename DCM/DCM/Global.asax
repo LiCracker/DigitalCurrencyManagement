51060faf14aa91655d4392cf6cf137c71fcc3662
Li Ma <scontdiplex@outlook.com>
2018-04-18 19:22:49 -0700

    initial

---
+++ DCM/DCM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DCM.Global" Language="C#" %>
---
+++ DCM/src/DCM/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DCM.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
